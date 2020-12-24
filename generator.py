e4215987e3a76d9ec6a5fbc68f655ec764df02fc
Christian Winter <christian.winter1990@gmail.com>
2020-12-24 22:28:43 +0100

    Fix code style

--- generator.py
+++ generator.py
@@ -94,13 +94,13 @@ def add_cpu_entries(data):
 def _get_cpu_entries():
     entries = {}
 
-    cpus = psutil.cpu_percent(percpu = True)
+    cpus = psutil.cpu_percent(percpu=True)
     counter = 0
     spacing = len(str(len(cpus))) + 1
     for cpu_load in cpus:
         entry = create_category_entry(cpu_load, " %", 0, 100)
         entries[f"CPU{str(counter).rjust(spacing)}"] = entry
-        counter +=1
+        counter += 1
 
     return entries
 
